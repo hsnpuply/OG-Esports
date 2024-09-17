b46457ae6289eadf18b282628e5563d2b4956dfe
Hassan Barati <48393874+hsnpuply@users.noreply.github.com>
2024-09-17 15:02:40 +0330

    fixing file + add fav icon

--- og-esport/src/App.vue
+++ og-esport/src/App.vue
@@ -14,15 +14,15 @@ const scroll_top = ref('')
 
 const scroll_show = () => {
     if (window.scrollY > 140) {
-    scroll_top.value.classList.add('morad')
-    scroll_top.value.classList.remove('hideScroll')
+        scroll_top.value.classList.add('morad')
+        scroll_top.value.classList.remove('hideScroll')
     } else {
         scroll_top.value.classList.add('hideScroll')
-  }
+    }
 }
 
 const goToTop = () => {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
 
@@ -30,8 +30,6 @@ onMounted(() => {
     window.addEventListener('scroll', scroll_show)
 });
 
-
-      const arandom= Math.random().toString(36).substr(2, 9); // generates a random 9-character string
 
 
 
@@ -41,11 +39,9 @@ onMounted(() => {
     <div class="font-titilium scroll-sm relative">
 
         <Heading />
-        <Hero_Banner_Section/>
-            <router-view/>
-
-        <Home_main/>
-        <Footer/>
+        <Hero_Banner_Section />
+        <Home_main />
+        <Footer />
         <!-- <div class="fixed bottom-16 right-8 bg-green-800 p-3 rounded-full text-2xl border-4 border-white text-white z-[100]">
             <p class="sm:hidden ">Tony</p>
             <p class="hidden sm:block md:hidden">Small</p>
@@ -54,23 +50,18 @@ onMounted(() => {
             <p class="hidden xl:block 2xl:hidden">Xl</p>
             <p class="hidden 2xl:block">2XL</p>
         </div> -->
-        <div class="scroll_top flex items-center justify-center bg-navbar_bg border-2 border-primary_color2 " ref="scroll_top" @click="goToTop()">
-            <img width="28" height="28" :src="arrowUp" class="select-none rotate-[-45deg]"/>
+        <div class="scroll_top flex items-center justify-center bg-navbar_bg border-2 border-primary_color2 "
+            ref="scroll_top" @click="goToTop()">
+            <img width="28" height="28" :src="arrowUp" class="select-none rotate-[-45deg]" />
         </div>
-            <RouterLink to="/test">Go to Test</RouterLink>
-            <br>
-            <RouterLink :to="{ name: 'Jobs' }">Go to jobs</RouterLink>
-            <br>
-      <Router-link :to="{ name: 'JobsDetail', params: { slugs: arandom } }">Job Detail</Router-link>
 
-    
 
     </div>
 
 </template>
 
 <style scoped>
-.scroll_top{
+.scroll_top {
     position: fixed;
     bottom: 40px;
     right: -100px;
@@ -82,12 +73,14 @@ onMounted(() => {
     cursor: pointer;
     font-size: 1.5rem;
 }
-.morad{
+
+.morad {
     right: 30px;
     transition: all .7s ease-in-out;
     opacity: 1;
 }
-.hideScroll{
+
+.hideScroll {
     right: -100px;
     opacity: 0;
 
