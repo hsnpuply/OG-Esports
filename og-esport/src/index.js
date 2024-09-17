2130871dafbb5b7d3b1bd62f55bb0336dab3eeec
Hassan Barati <48393874+hsnpuply@users.noreply.github.com>
2024-09-17 16:58:56 +0330

    removing junk files

--- og-esport/src/index.js
+++ og-esport/src/index.js
@@ -1,13 +1,7 @@
-import Test from '../src/components/footer.vue'
-import Jobs from '../src/components/jobs/jobs.vue'
-import JobsDetail from '../src/components/jobs/jobsDetail.vue'
+
 import { createMemoryHistory, createRouter } from 'vue-router'
 
 const routes = [
-  { path: '/test',name:'test', component: Test },
-  { path: '/jobs',name:'Jobs', component: Jobs },
-//   { path: '/jobs/id/:slugs',name:'JobsDetail', component: JobsDetail },
-  { path: '/:slugs',name:'JobsDetail', component: JobsDetail },
 ]
 
 const router = createRouter({
